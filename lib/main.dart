e83f7aea902cc426fe72098aee199284e4bd1acf
jodisaputra <33221764+jodisaputra@users.noreply.github.com>
2021-04-23 21:50:48 +0700

    Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:findcozy/screens/main/main_screen.dart';
+import 'package:findcozy/screens/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +10,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MainScreen(),
+      home: SplashScreen(),
     );
   }
 }
---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,50 @@
+import 'package:findcozy/screens/sign_in/sign_in_screen.dart';
+import 'package:findcozy/theme.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    startSplashScreen();
+  }
+
+  startSplashScreen() async {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, () {
+      Get.off(SignInScreen());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: softBlue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/image/logo.png',
+              width: 150,
+              height: 150,
+            ),
+            SizedBox(height: 30),
+            SpinKitWave(
+              color: white,
+              size: 30,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
