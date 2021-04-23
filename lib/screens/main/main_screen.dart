7094560096ac1b40ca7b43b5d1ee1f6b3f8100c6
jodisaputra <33221764+jodisaputra@users.noreply.github.com>
2021-04-23 20:50:15 +0700

    Bottom Navigation Bar

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,3 +1,8 @@
+import 'package:findcozy/screens/booking/booking_screen.dart';
+import 'package:findcozy/screens/favorite/favorite_screen.dart';
+import 'package:findcozy/screens/home/home_screen.dart';
+import 'package:findcozy/screens/profile/profile_screen.dart';
+import 'package:findcozy/theme.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -6,8 +11,81 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  PageController _pageController = PageController();
+  List<Widget> _screens = [
+    HomeScreen(),
+    FavoriteScreen(),
+    BookingScreen(),
+    ProfileScreen()
+  ];
+
+  // deklarasikan selectedIndex;
+  int _selectedIndex = 0;
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void _onItemTapped(int selectedIndex) {
+    _pageController.jumpToPage(selectedIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: PageView(
+          controller: _pageController,
+          children: _screens,
+          onPageChanged: _onPageChanged,
+          physics: NeverScrollableScrollPhysics(),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _onItemTapped,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: _selectedIndex == 0 ? lightBlue : grey,
+              ),
+              title: Text(
+                'Cari',
+                style: TextStyle(color: _selectedIndex == 0 ? lightBlue : grey),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+                color: _selectedIndex == 1 ? lightBlue : grey,
+              ),
+              title: Text(
+                'Favorite',
+                style: TextStyle(color: _selectedIndex == 1 ? lightBlue : grey),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home_work,
+                color: _selectedIndex == 2 ? lightBlue : grey,
+              ),
+              title: Text(
+                'Booking',
+                style: TextStyle(color: _selectedIndex == 2 ? lightBlue : grey),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person_rounded,
+                color: _selectedIndex == 3 ? lightBlue : grey,
+              ),
+              title: Text(
+                'Profile',
+                style: TextStyle(color: _selectedIndex == 3 ? lightBlue : grey),
+              ),
+            )
+          ],
+        ));
   }
 }
