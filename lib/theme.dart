dbe7ce677a99a2340b1475f4b7f1cfe3b54ad8a7
jodisaputra <33221764+jodisaputra@users.noreply.github.com>
2021-04-23 20:04:24 +0700

    setup screen

---
+++ lib/theme.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color darkBlue = Color(0xff143656);
+Color lightBlue = Color(0xff13497B);
+Color softBlue = Color(0xff46D0D9);
+Color white = Color(0xffFFFFFF);
+Color black = Color(0xff000000);
+Color red = Color(0xffFF0505);
+Color grey = Color(0xffB2B2B2);
+Color lightGrey = Color(0xfffafff7);
+
+TextStyle darkBlueTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: darkBlue,
+);
+
+TextStyle lightBlueTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: lightBlue,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: white,
+);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: black,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: grey,
+);
