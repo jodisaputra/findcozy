aaa5307c7cc50af54a78ff37d2da92febb265c34
jodisaputra <33221764+jodisaputra@users.noreply.github.com>
2021-04-24 01:06:55 +0700

    Sign In Screen for Admin and User

--- lib/screens/sign_in/sign_in_screen.dart
+++ lib/screens/sign_in/sign_in_screen.dart
@@ -1,4 +1,8 @@
+import 'package:findcozy/screens/sign_in_admin/sign_in_admin.dart';
+import 'package:findcozy/screens/sign_in_user/sign_in_user.dart';
+import 'package:findcozy/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SignInScreen extends StatefulWidget {
   @override
@@ -9,7 +13,85 @@ class _SignInScreenState extends State<SignInScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Sign In Screen')),
+      body: SafeArea(
+        child: Container(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: 25,
+              horizontal: 25,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Login Akun',
+                  style: darkBlueTextStyle.copyWith(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 30),
+                Text(
+                  'Saya ingin login akun sebagai',
+                  style: darkBlueTextStyle.copyWith(fontSize: 15),
+                ),
+                SizedBox(height: 30),
+                Row(
+                  children: [
+                    Expanded(
+                      child: InkWell(
+                        onTap: () {
+                          Get.to(SignInAdmin());
+                        },
+                        child: Container(
+                          height: 50,
+                          color: softBlue,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Pemilik Kos',
+                                style: darkBlueTextStyle.copyWith(
+                                    fontSize: 15, color: white),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: InkWell(
+                        onTap: () {
+                          Get.to(SignInUser());
+                        },
+                        child: Container(
+                          color: darkBlue,
+                          height: 50,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Pencari Kos',
+                                style: darkBlueTextStyle.copyWith(
+                                  fontSize: 15,
+                                  color: white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/screens/sign_in_user/sign_in_user.dart
@@ -0,0 +1,84 @@
+import 'package:findcozy/theme.dart';
+import 'package:flutter/material.dart';
+
+class SignInUser extends StatefulWidget {
+  @override
+  _SignInUserState createState() => _SignInUserState();
+}
+
+class _SignInUserState extends State<SignInUser> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.black),
+          elevation: 2,
+          backgroundColor: Colors.white,
+        ),
+        body: SafeArea(
+          child: Container(
+            color: white,
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 25,
+                horizontal: 25,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Login Pencari Kos',
+                    style: darkBlueTextStyle.copyWith(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 60),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Email *',
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: 'Password *',
+                    ),
+                  ),
+                  SizedBox(height: 70),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 50,
+                    child: MaterialButton(
+                      onPressed: () {},
+                      color: darkBlue,
+                      child: Text(
+                        'Login',
+                        style: whiteTextStyle.copyWith(fontSize: 16),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Belum punya akun findcozy?',
+                        style: greyTextStyle.copyWith(fontSize: 14),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Daftar Sekarang',
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
